b010d3fcbcaa02071791377000d3a0740b316651
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-05-09 12:55:14 +0900

    Update issue-A.js

--- issue-A.js
+++ issue-A.js
@@ -1,7 +1,9 @@
-// modify this file to change issue-A location
-// modify this file to change issue-A location
-// modify this file to change issue-A location
-
+function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
+    var arr = [1, 2, 3];
+    if (x < 0) {
+        arr[x] = 3;
+    }
+}
 function CWE_398() { // IDENTICAL_BRANCHES
     if (x >= 0) {
         y = x;
