c645e91fc67250d3f7eaef9342e251f63ca2e2e0
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-05-20 20:01:39 +0900

    Update issue-A.js

--- issue-A.js
+++ issue-A.js
@@ -1,11 +1,2 @@
 // comment line 1
 // comment line 2
-function CWE_398() { // IDENTICAL_BRANCHES
-    if (x >= 0) {
-        y = x;
-    } else {
-        y = x;
-    }
-}
-// comment line
-// comment line
