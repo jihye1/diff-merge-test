59708adf5f0c57a1bcec48e004ef69137143434d
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-05-24 15:02:17 +0900

    Update cwe.js

--- cwe.js
+++ cwe.js
@@ -80,3 +80,9 @@ function CWE_670(x) { // STRAY_SEMICOLON
     }
 }
 
+function FUNC_1() {
+    var arr = [1, 2, 3];
+    if (x < 0) {
+        arr[x] = 3; // ARRAY_INDEX_NEGATIVE alarm: x is negative but is used as array index.
+    }
+}
