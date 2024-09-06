bbe49c5d607f05e28c42caf32b76b38094029be1
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-09-06 16:26:25 +0900

    Update issue-A.js

--- issue-A.js
+++ issue-A.js
@@ -7,6 +7,12 @@
 //     }
 // }
 
+
+function CWE_482_665() { // UNUSED_EXPR
+    this.foo + 42; // deepscan-disable-line UNUSED_EXPR
+}
+
+
 function CWE_480_481() { // BAD_ASSIGN_IN_CONDITION
     var x = -1;
     if (x = -1) console.log('Error!', x);
