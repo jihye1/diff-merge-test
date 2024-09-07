c67146f146cbb6eb620e3270aa7806747c37e61c
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-09-08 08:20:36 +0900

    Update issue-B.js

--- issue-B.js
+++ issue-B.js
@@ -6,3 +6,10 @@ function CWE_398() { // IDENTICAL_BRANCHES
         y = x;
     }
 }
+
+function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
+    var arr = [1, 2, 3];
+    if (x < 0) {
+        arr[x] = 3;
+    }
+}
