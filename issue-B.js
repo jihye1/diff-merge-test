7546709482cfbd56061f0efbb28afd5bf663b0fe
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-09-10 14:13:14 +0900

    Update issue-B.js

--- issue-B.js
+++ issue-B.js
@@ -25,3 +25,10 @@ function CWE_480_481() { // BAD_ASSIGN_IN_CONDITION
     var x = -1;
     if (x = -1) console.log('Error!', x);
 }
+
+function CWE_480() { // BAD_BITWISE_OPERATOR
+    var obj = null;
+    if (obj & obj.prop) {
+        console.log(obj.prop);
+    }
+}
