3ce54568d2d8b748dc59c5c5c906e8921e75108f
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-09-10 14:18:57 +0900

    Update issue-A.js

--- issue-A.js
+++ issue-A.js
@@ -13,10 +13,10 @@ function CWE_482_665() { // UNUSED_EXPR
 }
 
 
-// function CWE_480_481() { // BAD_ASSIGN_IN_CONDITION
-//     var x = -1;
-//     if (x = -1) console.log('Error!', x);
-// }
+function CWE_480_481() { // BAD_ASSIGN_IN_CONDITION
+    var x = -1;
+    if (x = -1) console.log('Error!', x);
+}
 
 
 // comment line
