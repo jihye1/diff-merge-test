e6ac4bb331c61a529ca70c53b3ade5d12eaeed0d
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-09-12 10:33:56 +0900

    Update issue-B.js

--- issue-B.js
+++ issue-B.js
@@ -14,10 +14,10 @@ function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
     }
 }
 
-function CWE_476() { // NULL_POINTER
-    var obj;
-    // Add 1 line
-    var y = obj.x;
-    console.log(y);
-}
+// function CWE_476() { // NULL_POINTER
+//     var obj;
+//     // Add 1 line
+//     var y = obj.x;
+//     console.log(y);
+// }
 
