5b64c60c0d5c859c69f08da848cfa46f6e15d5ac
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-09-12 13:26:00 +0900

    Update issue-B.js

--- issue-B.js
+++ issue-B.js
@@ -1,11 +1,11 @@
 
-function CWE_398() { // IDENTICAL_BRANCHES
-    if (x >= 0) {
-        y = x;
-    } else {
-        y = x;
-    }
-}
+// function CWE_398() { // IDENTICAL_BRANCHES
+//     if (x >= 0) {
+//         y = x;
+//     } else {
+//         y = x;
+//     }
+// }
 
 // function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
 //     var arr = [1, 2, 3];
