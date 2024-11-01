af748e7c549b39940d679cc9956fe17535ea141a
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-11-01 15:24:49 +0900

    Update issue-B.js

--- issue-B.js
+++ issue-B.js
@@ -1,18 +1,18 @@
 
-// function CWE_398() { // IDENTICAL_BRANCHES
-//     if (x >= 0) {
-//         y = x;
-//     } else {
-//         y = x;
-//     }
-// }
-
-function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
-    var arr = [1, 2, 3];
-    if (x < 0) {
-        arr[x] = 3;
+function CWE_398() { // IDENTICAL_BRANCHES
+    if (x >= 0) {
+        y = x;
+    } else {
+        y = x;
     }
 }
+
+// function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
+//     var arr = [1, 2, 3];
+//     if (x < 0) {
+//         arr[x] = 3;
+//     }
+// }
 
 function CWE_476() { // NULL_POINTER
     var obj;
