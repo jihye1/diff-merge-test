f96df5428a387f49433300ec213976ce7404a875
Jihye Won <jihye.won1@samsung.com>
2024-11-05 14:46:20 +0900

    test
    Signed-off-by: Jihye Won <jihye.won1@samsung.com>

--- issue-A.js
+++ issue-A.js
@@ -1,10 +1,10 @@
-// function CWE_398() { // IDENTICAL_BRANCHES
-//     if (x >= 0) {
-//         y = x;
-//     } else {
-//         y = x;
-//     }
-// }
+ function CWE_398() { // IDENTICAL_BRANCHES
+     if (x >= 0) {
+         y = x;
+     } else {
+         y = x;
+     }
+ }
 
 
 function CWE_482_665() { // UNUSED_EXPR
