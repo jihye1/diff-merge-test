4ee3a9372329e4f57b41bfcf90166118f01e6c28
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-11-05 16:59:10 +0900

    Update issue-B.js

--- issue-B.js
+++ issue-B.js
@@ -14,11 +14,19 @@ function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
     }
 }
 
-function CWE_476() { // NULL_POINTER
-    var obj;
-    // Add 1 line
-    var y = obj.x;
-    console.log(y);
+// function CWE_476() { // NULL_POINTER
+//     var obj;
+//     // Add 1 line
+//     var y = obj.x;
+//     console.log(y);
+// }
+
+function CWE_489_569_570_571() { // CONSTANT_CONDITION
+    var x = "Fo";
+    var y = x + "Bar";
+    if (y === "FooBar") {
+        console.log('Do something');
+    }
 }
 
 function CWE_489_569_570_571() { // CONSTANT_CONDITION
@@ -29,10 +37,12 @@ function CWE_489_569_570_571() { // CONSTANT_CONDITION
     }
 }
 
-// function CWE_489_569_570_571() { // CONSTANT_CONDITION
-//     var x = "Fo";
-//     var y = x + "Bar";
-//     if (y === "FooBar") {
-//         console.log('Do something');
-//     }
-// }
+
+
+function CWE_489_569_570_571() { // CONSTANT_CONDITION
+    var x = "Fo";
+    var y = x + "Bar";
+    if (y === "FooBar") {
+        console.log('Do something');
+    }
+}
