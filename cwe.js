bafa6247d9be376e974843427dcd78d39ea80831
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-11-06 14:30:30 +0900

    Update cwe.js

--- cwe.js
+++ cwe.js
@@ -80,9 +80,9 @@ function CWE_670(x) { // STRAY_SEMICOLON
     }
 }
 
-function CWE_670(x) { // STRAY_SEMICOLON
-     while (++x <= 10) ;
-     {
-         sum += x;
-     }
- }
+// function CWE_670(x) { // STRAY_SEMICOLON
+//      while (++x <= 10) ;
+//      {
+//          sum += x;
+//      }
+//  }
