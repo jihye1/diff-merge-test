a68b101da3cad112048c7cd8392f876f7af7294f
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-12-20 16:40:26 +0900

    Update cherry-pick.js

--- cherry-pick.js
+++ cherry-pick.js
@@ -5,3 +5,10 @@ function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
         arr[x] = 3;
     }
 }
+
+function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
+    var arr = [1, 2, 3];
+    if (x < 0) {
+        arr[x] = 3;
+    }
+}
