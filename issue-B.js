15e5bca0d285fdea0176b55b876c140297aa2e44
jihye1 <36979920+jihye1@users.noreply.github.com>
2024-12-24 09:50:11 +0900

    Update issue-B.js

--- issue-B.js
+++ issue-B.js
@@ -43,3 +43,10 @@ function CWE_476() { // NULL_POINTER
     var y = obj.x;
     console.log(y);
 }
+// pr-feature-3
+function CWE_129(x) { // ARRAY_INDEX_NEGATIVE
+    var arr = [1, 2, 3];
+    if (x < 0) {
+        arr[x] = 3;
+    }
+}
